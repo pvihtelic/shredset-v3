eac91b48a29bbcf521efa3282dc9ffd4201347c1
pvihtelic <patrick.vihtelic@gmail.com>
2012-11-26 13:20:33 -0600

    updating search box back end

--- app/controllers/skis_controller.rb
+++ app/controllers/skis_controller.rb
@@ -2,19 +2,41 @@ class SkisController < ApplicationController
   # GET /skis
   # GET /skis.json
   def index
-    @skis = Ski.all
 
-    ski_type = params[:ski_type]
+    if params[:ski].blank?
 
-    @skis = @skis.where(:ski_type => ski_type) if ski_type.present?
+      @skis = Ski.all
 
-    gender = params[:gender]
+    else
 
-    @skis = @skis.where(:gender => gender) if gender.present?
+      ski_type = params[:ski][:ski_type]
+      gender = params[:ski][:gender]
+      ability_level = params[:ski][:ability_level]
 
-    ability_level = params[:ability_level]
+      @skis = Ski.where(:ski_type => ski_type[1], :gender => gender[1], :ability_level => ability_level[1])
+      # if !ski_type[1].blank?
 
-    @skis = @skis.where(:ability_level => ability_level) if ability_level.present?
+
+
+      # @skis2 = Ski.where(:gender => gender[1]) if !gender[1].blank?
+
+      
+
+      # @skis3 = Ski.where(:ability_level => ability_level[1]) if !ability_level[1].blank?
+
+      # @skis = []
+      # if !@skis1.nil?
+      #   @skis.concat(@skis1)
+      # end
+      # if !@skis2.nil? 
+      #   @skis.concat(@skis2)
+      # end
+      # if !@skis3.nil?
+      #   @skis.concat(@skis3)
+      # end
+    end
+
+
 
     respond_to do |format|
       format.html # index.html.erb
