60570d461beda3a3850a1ae921c0bb413ec1fdfb
pvihtelic <patrick.vihtelic@gmail.com>
2012-11-29 21:17:20 -0600

    homepage carousel db backed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -24,6 +24,7 @@ class PagesController < ApplicationController
         @park_and_pipe_top_skis = []
         @alpine_touring_top_skis = []
         @all_mountain_ski_packages_top_skis = []
+        @carving_top_skis = []
 
         top_reviews.each do |review|  
           all_mountain_top_ski = Ski.where(:ski_type => "All Mountain Skis", :id => review.ski_id)
@@ -41,30 +42,26 @@ class PagesController < ApplicationController
             @ski_object = ski_object
             @powder_top_skis << @ski_object
           end
-          # park_and_pipe_top_ski = Ski.where(:ski_type => "Park & Pipe Skis", :id => review.ski_id)
-          # park_and_pipe_top_ski.each do |ski_object|
-          #   @ski_object = ski_object
-          #   if nil?
-          #   else
-          #   @park_and_pipe_top_skis << @ski_object
-          #   end
-          # end
-          # alpine_touring_top_ski = Ski.where(:ski_type => "Park & Pipe Skis", :id => review.ski_id)
-          # alpine_touring_top_ski.each do |ski_object|
-          #   @ski_object = ski_object
-          #   if nil?
-          #   else
-          #   @alpine_touring_top_skis << @ski_object
-          #   end
-          # end
-          # all_mountain_ski_packages_top_ski = Ski.where(:ski_type => "Park & Pipe Skis", :id => review.ski_id)
-          # all_mountain_ski_packages_top_ski.each do |ski_object|
-          #   @ski_object = ski_object
-          #   if nil?
-          #   else
-          #   @all_mountain_ski_packages_top_skis << @ski_object
-          #   end
-          # end
+          park_and_pipe_top_ski = Ski.where(:ski_type => "Park & Pipe Skis", :id => review.ski_id)
+          park_and_pipe_top_ski.each do |ski_object|
+            @ski_object = ski_object
+            @park_and_pipe_top_skis << @ski_object
+          end
+          alpine_touring_top_ski = Ski.where(:ski_type => "Park & Pipe Skis", :id => review.ski_id)
+          alpine_touring_top_ski.each do |ski_object|
+            @ski_object = ski_object
+            @alpine_touring_top_skis << @ski_object
+          end
+          all_mountain_ski_packages_top_ski = Ski.where(:ski_type => "Park & Pipe Skis", :id => review.ski_id)
+          all_mountain_ski_packages_top_ski.each do |ski_object|
+            @ski_object = ski_object
+            @all_mountain_ski_packages_top_skis << @ski_object
+          end
+          carving_top_ski = Ski.where(:ski_type => "Carving Skis", :id => review.ski_id)
+          carving_top_ski.each do |ski_object|
+            @ski_object = ski_object
+            @carving_top_skis << @ski_object
+          end
         end
     	end
 	end
