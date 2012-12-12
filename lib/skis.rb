c00f8ef30ee4dba3b2a6fba0e72373e5ae08728b
pvihtelic <patrick.vihtelic@gmail.com>
2012-12-11 21:20:09 -0600

    fixing years as ski types

--- lib/skis.rb
+++ lib/skis.rb
@@ -366,7 +366,7 @@ class Skis
 				@ski_type = "Park & Pipe Skis"
 			elsif @ski_type.include? "Powder"
 				@ski_type = "Big Mountain Skis"
-			elsif @ski_type == "2010"
+			elsif @ski_type == "2010" || @ski_type == "2012" || @ski_type == "2013"
 				@ski_type = "Ski Boards"
 			elsif @ski_type == "Yes"
 				@ski_type = "Ski Boards"
